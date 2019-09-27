bb1397a2091373dd8c3e134935e80e7abaa49ca7
Hubert Yang <offspring7890@yahoo.com>
2019-09-27 09:45:25 -0700

    finish routing requests

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('./logger')
 const { NODE_ENV } = require('./config');
+const bookmarksRouter = require('./bookmarks-router')
 
 const app = express();
 
@@ -15,9 +17,21 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+app.use('/bookmarks', bookmarksRouter);
+
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = app.get('Authorization');
+
+  if(!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path ${req.path}`)
+    return res.status(401).json({ error: 'Unauthorized Request' })
+  }
+});
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
